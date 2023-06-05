848da6058b69e752d8057bab837ab65a6049e731
Syaiful Imanudin <112718167+SaipulImdn@users.noreply.github.com>
2023-06-05 20:30:57 +0700

    Add files via upload

---
+++ php/belajar-php-dasar/aritmatika.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+$result = $a + $b;
+var_dump($result);
+
+$resultNegative = -$result;
+var_dump($resultNegative);
+
+$resultModulo = 100 % 0;
+var_dump($resultModulo);
---
+++ php/belajar-php-dasar/penuugasan.php
@@ -0,0 +1,13 @@
+<?php
+
+$total = 0;
+
+$fruit = 10000;
+$chicken = 2000;
+$orangejuice = 5000;
+
+$total += $fruit;
+$total += $chicken;
+$total += $orangejuice;
+
+var_dump($total);
