1bea02b1618d714509d9349a8a2c0d531b691d4b
Syaiful Imanudin <112718167+SaipulImdn@users.noreply.github.com>
2023-06-05 21:52:37 +0700

    Add files via upload

---
+++ dot.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = "Syaiful Imanudin";
+
+echo "Nama : ". $name . PHP_EOL;
+echo "Value : ". 100 . PHP_EOL;
+
+$valueString = (boolean)100;
+var_dump($valueString);
+
+$valueInt = (int)"100";
+var_dump($valueInt);
+
+$valueFloat = (float)"1.01";
+var_dump($valueFloat);
