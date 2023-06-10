1ff6ad6aee3c762880a112d75b952e070951fc3f
Syaiful Imanudin <112718167+SaipulImdn@users.noreply.github.com>
2023-06-10 12:51:21 +0700

    Add files via upload

---
+++ php/belajar-php-dasar/loop.php
@@ -0,0 +1,6 @@
+<?php
+
+for ($counter = 1 ; $counter <= 10;  $counter++):
+    echo "Hallo -$counter" .PHP_EOL;
+
+endfor;
---
+++ php/belajar-php-dasar/vari.php
@@ -0,0 +1,14 @@
+<?php
+
+function fou()
+{
+    echo "fou" .PHP_EOL;
+}
+
+function bar()
+{
+    echo "bar" .PHP_EOL;
+}
+
+$functionyangakandipanggil = "fou";
+$functionyangakandipanggil  ();
